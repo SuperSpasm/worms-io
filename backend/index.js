a81d7bd16f4afc58b6ddc2b32de5913d88932b53
VadimGut <vadimku@gmail.com>
2019-09-13 02:03:26 +0300

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const _ = require('lodash');
-const usersEvents = require('./events/Users');
 
 app.get('/', function(req, res) {
     res.send('Hello world!');
